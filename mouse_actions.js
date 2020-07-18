aa6115348871680b6f8155cf9642fea88518be84
Raissa Pimentel <raissabmpimentel@gmail.com>
2020-07-18 20:56:18 -0300

    First version of drag

--- mouse_actions.js
+++ mouse_actions.js
@@ -1,8 +1,6 @@
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
 
-
-
 function onMouseMove( event ) {
 
 	// calculate mouse position in normalized device coordinates
@@ -11,8 +9,54 @@ function onMouseMove( event ) {
 	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
     mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
     
-
+    // update the picking ray with the camera and mouse position
+    raycaster.setFromCamera( mouse, camera );
 }
+
+var drag_obj = [];
+var dragControls;
+
+function init_drag() {
+    dragControls = new THREE.DragControls(drag_obj, camera, renderer.domElement);
+    dragControls.transformGroup = false;
+    dragControls.addEventListener('dragstart', function(event) {
+      controls.enabled = false;
+    });
+    dragControls.addEventListener('dragend', function(event) {
+      controls.enabled = true;
+    });
+
+  }
+
+init_drag();
+
+function raycast(event) {
+    drag_obj.length = 0;
+    var list_obj = [];
+    var group_obj = new THREE.Group();
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    
+    // update the picking ray with the camera and mouse position
+    raycaster.setFromCamera( mouse, camera );
+
+     intersects = raycaster.intersectObjects( scene.children, true );
+     if ( INTERSECTED && valueInArray(INTERSECTED.name,list_names))
+     {
+             scene.traverse(function(child) {
+                 if (child.name === INTERSECTED.name) {
+                    drag_obj.push(child); //apply same material to all meshes
+                    //list_obj.push(child);
+                    //console.log(child.clone());
+                 }
+               });
+               
+               //drag_obj.push(group_obj);
+     } 
+        
+}
+
+window.addEventListener( 'mousedown', raycast, false );
 
 function valueInArray(value, vec) {
     var result = false;
@@ -31,17 +75,14 @@ function valueInArray(value, vec) {
 /// TESTE
 var INTERSECTED = null;
 var list_names = ["lampada","sofa 1","sofa 2","batente","mesa"];
-var list_materials = [];
+
 
 function intersec() {
-    
-    // update the picking ray with the camera and mouse position
-    raycaster.setFromCamera( mouse, camera );
     
 	// calculate objects intersecting the picking ray
     var intersects = raycaster.intersectObjects( scene.children, true);
     
-    var mat = new THREE.MeshLambertMaterial({color: 0xffbfbf});
+    var mat = new THREE.MeshLambertMaterial({color: 0xffa1a1});
 
     // Baseado em https://stackoverflow.com/questions/38314521/change-color-of-mesh-using-mouseover-in-three-js
     if ( intersects.length > 0 )
